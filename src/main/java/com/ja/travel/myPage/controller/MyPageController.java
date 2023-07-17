2ba36eef3b0b69e189407b6e691873cc6f84f9c2
정인철 <waz6432@naver.com>
2023-07-17 14:40:17 +0900

    ㅇ

--- src/main/java/com/ja/travel/myPage/controller/MyPageController.java
+++ src/main/java/com/ja/travel/myPage/controller/MyPageController.java
@@ -1,18 +1,14 @@
 package com.ja.travel.myPage.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyPageController {
-
-	@RequestMapping("/myPage")
-	public String myPage(Model model, HttpSession session) {
-
-		return "my/myPage";
-	}
+//
+//	@RequestMapping("/myPage")
+//	public String myPage(Model model, HttpSession session) {
+//
+//		return "my/myPage";
+//	}
 
 }
